60bd0230da82624709f066d38261e6758a945fcd
Hari Shankar Gurusamy <harishankx@gmail.com>
2020-08-13 01:51:35 +0530

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+p "Creating Roles"
+
+customer_role = Role.create(role_name: "Customer", role_code: "Emp_CX")
+manager_role = Role.create(role_name: "Manager", role_code: "Emp_MG")
+
+p "Roles created"
+
+p "User Creation"
+manager_user = User.create(user_name: "manager_user_001",
+                           email: "manager_user_001@gmail.com",
+                           password: 123456,
+                           date_of_birth: "1992-10-02",
+                           gender: "male",
+                           phone_number: 9192093022,
+                           role_id: manager_role.id)
+
+customer_user = User.create(user_name: "customer_user_001",
+                           email: "customer_user_001@gmail.com",
+                           password: 123456,
+                           date_of_birth: "1992-10-02",
+                           gender: "male",
+                           phone_number: 9192093022,
+                           role_id: customer_role.id)
+
+p "User creation done..."
